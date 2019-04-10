f455f4c0e453101fea9f43493c8b5416c75e813c
Alex <akac92@icloud.com>
2019-04-10 16:50:36 -0700

    Define game colors
    :art: Can this be done in the Rust module in
    an efficient manner?
    
    - Configuration should happen on the rust end as much as possible.
    
    :beer: - Lekrow

--- www/index.js
+++ www/index.js
@@ -1,5 +1,9 @@
 import { Universe, Cell } from "game-of-life";
 
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
 
 const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
