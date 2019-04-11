62463ea3619497072c1cbf378db2287f16697f18
AlexLeCrow <akac92@uw.edu>
2019-04-11 16:41:45 -0700

    :construction: update tests

--- src/lib.rs
+++ src/lib.rs
@@ -122,18 +122,6 @@ impl Universe {
 
         count
     }
-
-    pub fn width(&self) -> u32 {
-        self.width
-    }
-
-    pub fn height(&self) -> u32 {
-        self.height
-    }
-
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
-    }
 }
 
 /// Public methods, exported to JavaScript.
--- tests/web.rs
+++ tests/web.rs
@@ -1,19 +1,13 @@
 //! Test suite for the Web and headless browsers.
-
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
 
-extern crate wasm_game_of_life;
-use wasm_game_of_life::Universe;
+extern crate game_of_life;
+use game_of_life::Universe;
 
 wasm_bindgen_test_configure!(run_in_browser);
-
-#[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
-}
 
 #[cfg(test)]
 pub fn input_spaceship() -> Universe {
@@ -42,7 +36,7 @@ pub fn test_tick() {
     // after one tick in our universe.
     let expected_universe = expected_spaceship();
 
-    //// Call `tick` and then see if the cells in the `Universe`s are the same.
+    // Call `tick` and then see if the cells in the `Universe`s are the same.
     input_universe.tick();
     assert_eq!(&input_universe.get_cells(), &expected_universe.get_cells());
 }
