42ae9c0e367a4d922d676d1ca7b7a10b90e07c0d
Alex <akac92@icloud.com>
2019-04-10 18:28:28 -0700

    Optimize the shader performance by avoiding fillStyle changes

--- www/index.js
+++ www/index.js
@@ -7,7 +7,6 @@ import {
 } from "game-of-life/game_of_life_bg";
 
 const CELL_SIZE = 5; // px
-const CELL_SIZE_ = CELL_SIZE + 1;
 const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
@@ -17,12 +16,10 @@ const universe = Universe.new();
 const width = universe.width();
 const height = universe.height();
 
-// Get 2D context of our sized canvas DOM object
-const canvas = document.getElementById("game-of-life-canvas");
 // Give the canvas room for all of our cells and a 1px border around each of them.
 const canvas = document.getElementById("game-of-life-canvas");
-canvas.height = CELL_SIZE_ * height + 1;
-canvas.width = CELL_SIZE_ * width + 1;
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const ctx = canvas.getContext('2d');
 
@@ -116,14 +113,14 @@ const drawGrid = () => {
 
   // Vertical lines.
   for (let i = 0; i <= width; i++) {
-    ctx.moveTo(i * CELL_SIZE_ + 1, 0);
-    ctx.lineTo(i * CELL_SIZE_ + 1, CELL_SIZE_ * height + 1);
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
   }
 
   // Horizontal lines.
   for (let j = 0; j <= height; j++) {
-    ctx.moveTo(0,                      j * CELL_SIZE_ + 1);
-    ctx.lineTo(CELL_SIZE_ * width + 1, j * CELL_SIZE_ + 1);
+    ctx.moveTo(0,                      j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
   }
 
   ctx.stroke();
@@ -133,11 +130,47 @@ const getIndex = (row, column) => {
   return row * width + column;
 };
 
-    drawGrid();
-    drawCells();
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
 
-    requestAnimationFrame(renderLoop);
-}
+  ctx.beginPath();
 
-// Start loop
-requestAnimationFrame(renderLoop);
+  // Alive cells.
+  ctx.fillStyle = ALIVE_COLOR;
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+      if (cells[idx] !== Cell.Alive) {
+        continue;
+      }
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  // Dead cells.
+  ctx.fillStyle = DEAD_COLOR;
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+      if (cells[idx] !== Cell.Dead) {
+        continue;
+      }
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
