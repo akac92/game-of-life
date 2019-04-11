13b3ccc135a3534d5f7ba6328e94ed9c7acf0b11
Alex <akac92@icloud.com>
2019-04-10 18:15:17 -0700

    Implement fps class

--- www/index.js
+++ www/index.js
@@ -1,5 +1,10 @@
-import { Universe, Cell } from "game-of-life";
-import { memory } from "game-of-life/game_of_life_bg";
+import {
+  Universe,
+  Cell
+} from "game-of-life";
+import {
+  memory
+} from "game-of-life/game_of_life_bg";
 
 const CELL_SIZE = 5; // px
 const CELL_SIZE_ = CELL_SIZE + 1;
@@ -15,9 +20,54 @@ const height = universe.height();
 // Get 2D context of our sized canvas DOM object
 const canvas = document.getElementById("game-of-life-canvas");
 // Give the canvas room for all of our cells and a 1px border around each of them.
+const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = CELL_SIZE_ * height + 1;
 canvas.width = CELL_SIZE_ * width + 1;
+
 const ctx = canvas.getContext('2d');
+
+const fps = new class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = 1 / delta * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+  }
+};
 
 // Draw equal-distant overlapping vertical and horizontal lines to create the grid overlay
 const drawGrid = () => {
